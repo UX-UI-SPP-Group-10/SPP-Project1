853f4810583721a1e2e3f46947c83e045f3677e7
s225042 <144696133+s225042@users.noreply.github.com>
2024-10-31 10:02:52 +0100

    start

--- app/src/main/java/com/sppProject/app/MainActivity.kt
+++ app/src/main/java/com/sppProject/app/MainActivity.kt
@@ -26,9 +26,193 @@ class MainActivity : ComponentActivity() {
 
         // Set the content of the activity
         setContent {
-            SimpleBuyerApp(buyerFetcher)
+            Logindpage(buyerFetcher)
         }
     }
+}
+
+@Composable
+fun Logindpage(buyerFetcher: BuyerFetcher) {
+    var logOn by remember { mutableStateOf(false) }
+    var newUser by remember { mutableStateOf(false) }
+    var name by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    if (!newUser) {
+        Column(modifier = Modifier.padding(top = 16.dp)) {
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Enter Uesrname") }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Enter Password") },
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = { newUser = true }) {
+                Text("Creat profile")
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Button(
+                    onClick = {
+                        logOn = true
+                    },
+                    modifier = Modifier.align(Alignment.Center)
+                ) {
+                    Text("Log in")
+                }
+            }
+
+        }
+    }
+    else{
+        App(backToLogin = { newUser = false })
+    }
+}
+
+@Composable
+fun App(backToLogin: () -> Unit){
+    var retylerInfomation by remember { mutableStateOf(false) }
+    var youserInfomation by remember { mutableStateOf(false) }
+    var sendInfo by remember { mutableStateOf(false) }
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 32.dp))
+    {
+        RetylerInfo(
+            retylerInfomation = retylerInfomation,
+            onClick = {
+                retylerInfomation = !retylerInfomation
+                if(retylerInfomation) youserInfomation = false},
+            sendInfo = sendInfo
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        UserInfo(
+            youserInfomation = youserInfomation,
+            onClick = {
+                youserInfomation = !youserInfomation
+                if (youserInfomation) retylerInfomation = false
+            },
+            sendInfo = sendInfo
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp) // Optional padding from the edges
+    ) {
+        Button(
+            onClick = {
+                sendInfo = true
+            },
+            modifier = Modifier.align(Alignment.BottomEnd) // Aligns the button to the bottom-right
+        ) {
+            Text("Make Profile")
+        }
+
+        Button(onClick = backToLogin,
+            modifier = Modifier.align(Alignment.BottomStart)){
+            Text("Back")
+        }
+    }
+}
+
+@Composable
+fun RetylerInfo(
+    retylerInfomation: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    sendInfo: Boolean){
+    var company by remember { mutableStateOf("") }
+    var location by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Button(onClick = onClick) {
+        Text("Retailer")
+    }
+
+    if(retylerInfomation){
+        Column(modifier = Modifier.padding(top = 16.dp)){
+            TextField(
+                value = company,
+                onValueChange = { company = it },
+                label = { Text("Enter Company") }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = location,
+                onValueChange = { location = it },
+                label = { Text("Enter Location") }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Enter Password") },
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+    if(retylerInfomation && sendInfo){
+        //send infomation and gow to logind page
+    }
+}
+
+@Composable
+fun UserInfo(
+    youserInfomation: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    sendInfo: Boolean)
+{
+    var name by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Button(onClick = onClick) {
+        Text("User")
+    }
+    if(youserInfomation){
+        Column(modifier = Modifier.padding(top = 16.dp)){
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Enter Uesrname") }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Enter Password") },
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+
+    if(youserInfomation && sendInfo){
+        //send infomation and gow to logind page
+    }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
