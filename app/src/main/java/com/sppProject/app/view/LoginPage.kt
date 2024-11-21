db581893c44d8627e2439269a4019e343bcb5703
karlpedro <162471444+karlpedro@users.noreply.github.com>
2024-11-21 14:13:22 +0100

    userlogin textfield changes

--- app/src/main/java/com/sppProject/app/view/LoginPage.kt
+++ app/src/main/java/com/sppProject/app/view/LoginPage.kt
@@ -1,19 +1,25 @@
 package com.sppProject.app.view
 
+import android.R.attr.name
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -25,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sppProject.app.UserNavActions
 import com.sppProject.app.view.components.CustomButton
 import com.sppProject.app.view.components.CustomToggleButton
@@ -145,10 +152,40 @@ private fun UserTypeSelector(
 }
 
 @Composable
-private fun UsernameInputField(name: String, onNameChange: (String) -> Unit) {
-    TextField(
-        value = name,
-        onValueChange = onNameChange, // This should update the name state in LoginPage
-        label = { Text("Enter Username") }
-    )
+private fun UsernameInputField(
+    modifier: Modifier = Modifier,
+    borderWidth: androidx.compose.ui.unit.Dp = 1.dp,
+    borderColor: androidx.compose.ui.graphics.Color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f),
+    name: String,
+    onNameChange: (String) -> Unit,
+    shape: androidx.compose.ui.graphics.Shape = RoundedCornerShape(12.dp)){
+    Box(
+        modifier = modifier
+            .border(
+                BorderStroke(borderWidth, borderColor),
+                shape = shape
+            )
+    ) {
+        TextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = {
+                Text(
+                    text = "Username",
+                    fontSize = 14.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.primary,
+                unfocusedContainerColor = MaterialTheme.colorScheme.primary,
+                focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+                cursorColor = MaterialTheme.colorScheme.primary
+            ),
+            shape = shape,
+            singleLine = true,
+        )
+    }
+
 }
